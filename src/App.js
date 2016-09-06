c278c34e0e3af15ad794549492463e18ab4ca5e2
Pavel Sukhodolskii <pvlt.job@gmail.com>
2016-09-06 20:44:34 +0300

    Добавлен vkapi.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import './App.css';
-import RoadMap from './Map';
+import { Map, Marker} from 'yandex-map-react';
+import points from './data';
+
+const APPID = 5618842;
+const APPSECRET = 'R7AIAbXopL693wQeDsnp';
+const REDIRECT_URL = 'http://pvlt.test.com:3000';
+
+const vkontakte = require('vkontakte');
+
+let vk = vkontakte(APPID, APPSECRET);
+vk('users.get', { user_ids: 'pavlletto', fields: 'uid,first_name,photo'  }, function (err, user) {
+ console.log(user);
+});
+
+const mapStyles = {
+    float: 'left'
+};
 
 class App extends Component {
   render() {
-    return (
-    <RoadMap />
-    );
-  }
-}
-
+      return (
+          <Map style={mapStyles} height={'100%'}  width={'100%'}  center={[59.9304132,30.3332581]} zoom={10}>
+        {points.map(([lon,lat], i) =>  (
+            <Marker key={'marker_' + i} lat={lat} lon={lon} />
+        ))}
+        </Map>
+)}}
 export default App;
