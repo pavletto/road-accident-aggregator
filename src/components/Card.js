f8fab4fc2d206fe4349f3712569cebd66508bc1d
Pavel Sukhodolskii <pvlt.job@gmail.com>
2016-10-01 19:25:50 +0300

    При клике на маркер в компонент добавляется текст маркера

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,20 +4,34 @@ import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import './Card.css'
 
-const PointCard = () => (
-    <Card className="point-card">
-    <CardHeader
+export default class PointCard extends React.Component { 
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+              expanded: false
+        }
+    }
+
+    handleExpandChange = (expanded) => {
+        this.setState({expanded: expanded});
+    }
+
+    render() {
+        return (
+    
+    <Card className="point-card" expandable={false} >
+    <CardHeader id="point-card_date"
     title="Without Avatar"
     subtitle="Subtitle"
     />
-    <CardText>
+    <CardText id="point-card_text" >
     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
     Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
     Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
     Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
     </CardText>
     </Card>
+)}
+};
 
-);
-
-export default PointCard;
--- src/components/Points.js
+++ src/components/Points.js
@@ -1,5 +1,9 @@
 import React, { Component  } from 'react'
 import { Marker, MarkerLayout } from 'yandex-map-react'
+import FlatButton from 'material-ui/FlatButton';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import PointCard from './Card'
 
 const markerStyles = {
     width: '40px',
@@ -13,9 +17,16 @@ const markerStyles = {
 export default class Points extends Component {
     showTooltip (point, i) {
         !point.c ? point.c = true : point.c = !point.c;
-        point.c ?
-            document.getElementById('marker_tooltip'+i).className = " tooltip_clicked":
-            document.getElementById('marker_tooltip'+i).className = "";
+        var formatDate = function(dateString) {
+          
+             return dateString.replace(/^(\d{4})-(\d{2})-(\d{2})/, '$2/$3/$1');
+          //
+        };
+            document.getElementById('point-card_text').innerText = point.text 
+            // document.getElementById('point-card_date').innerText = formatDate(new Date(point.date))
+            document.getElementById('point-card_date').innerText = new Date(point.date *1000);
+            console.log(PointCard);
+           console.log(PointCard.state)
     }
     componentDidMount() {
        this.props.getPoints()
@@ -25,11 +36,6 @@ export default class Points extends Component {
         const _this = this 
         return (<div>{ points.map((point,i) => ( 
             <Marker onClick={function(){_this.showTooltip(point, i)}} key={'marker_' + i} lat={point.marker[0].lat} lon={point.marker[0].long} >
-                <MarkerLayout>
-                    <div id={"marker_tooltip" + i} style={markerStyles} data-tooltip={point.text}>
-                        <img src="http://loremflickr.com/40/40"/>
-                    </div>
-                </MarkerLayout>
             </Marker>
             ))
             }</div>) 
